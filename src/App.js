1a781dbcaa6cc4563570df6930a9eef1f32e9b49
harkirat4u <harkiratsingh051@gmail.com>
2021-03-21 16:56:17 -0400

    error solve

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-import { Modal } from './components/Modal';
-import { GlobalStyle } from './globalStyles';
+
 
 
 function App() {
